74421e2ee8ae23063c03a90c0b63bd08d4acd6e6
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-07 09:05:12 -0600

    added app bar and some style to the widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text'),
+        // control + space_bar to cycle through suggested arguments
+      ),
     );
   }
 }
