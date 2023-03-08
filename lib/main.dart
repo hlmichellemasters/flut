ddf06f4cea3f4fd478efe7ea3241c23b3eca23f3
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-08 08:31:54 -0600

    starting quiz

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void answerQuestion() {
+      print('Answer chosen!')
+    }
+
+    var questions = ['How', 'Why'];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is my default text'),
-        // control + space_bar to cycle through suggested arguments
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion()),
+              ElevatedButton(child: Text('Answer 2'), onPressed: answerQuestion()),
+              ElevatedButton(child: Text('Answer 3'), onPressed: answerQuestion()),
+            ],
+          )
+          // control + space_bar to cycle through suggested arguments
+          ),
     );
   }
 }
