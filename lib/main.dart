2f6c221be18c862180d8d189fe71aacf599aa572
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-09 08:55:07 -0600

    started using stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    void answerQuestion() {
-      print('Answer chosen!')
-    }
-
     var questions = ['How', 'Why'];
     return MaterialApp(
       home: Scaffold(
@@ -17,10 +27,18 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('The question!'),
-              ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion()),
-              ElevatedButton(child: Text('Answer 2'), onPressed: answerQuestion()),
-              ElevatedButton(child: Text('Answer 3'), onPressed: answerQuestion()),
+              Text(questions[_questionIndex]),
+              ElevatedButton(child: Text('Answer 1'), onPressed: _answerQuestion
+                  // print('Answer 1 chosen')
+                  ),
+              ElevatedButton(
+                  child: Text('Answer 2'),
+                  onPressed: () => print('Answer 2 chosen')),
+              ElevatedButton(
+                  child: Text('Answer 3'),
+                  onPressed: () => print('Answer 3 chosen')),
+              // pass function (answerQuestion) without parentheses after
+              // to not call right away and instead pass the function (pointer)
             ],
           )
           // control + space_bar to cycle through suggested arguments
