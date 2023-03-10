e2de1527900ceab4aef513b28fb22bfeb203ecad
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-10 08:08:08 -0600

    create stateless question widget

---
+++ lib/question.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  // const Question({Key key}) : super(key: key);
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
