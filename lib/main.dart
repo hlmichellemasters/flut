5246347504519d4d9dcf490d0e903d1382b8797b
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-13 08:35:43 -0500

    complete refactor of quiz into maps and widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,36 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': 'How?',
-      'answers': ['Mushies', 'Aya', 'D']
+      'questionText': 'Mood?',
+      'answers': [
+        {'text': 'Terrible', 'score': 0},
+        {'text': 'Okay', 'score': 5},
+        {'text': 'Awesome', 'score': 10},
+      ]
     },
     {
-      'questionText': 'Why?',
-      'answers': ['Sad', 'Mad', 'Glad']
+      'questionText': 'What?',
+      'answers': [
+        {'text': 'Sad', 'score': 0},
+        {'text': 'Mad', 'score': 5},
+        {'text': 'Glad', 'score': 10},
+      ]
     },
     {
-      'questionText': 'When',
-      'answers': ['Now', '2 Weeks', '2 Months']
+      'questionText': 'How long?',
+      'answers': [
+        {'text': 'Now', 'score': 0},
+        {'text': '2 Hours', 'score': 5},
+        {'text': 'All Day', 'score': 10},
+      ]
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
