9f12b2ff871fa364d46e62656247e0830ce46e5c
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-12 20:54:41 -0500

    break out new widgets for readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
 import './answer.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'How?',
       'answers': ['Mushies', 'Aya', 'D']
@@ -32,7 +33,7 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     }
   }
@@ -44,17 +45,12 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('My First App'),
           ),
-          body: _questionIndex < questions.length
-              ? Column(
-                  children: [
-                    Question(questions[_questionIndex]['questionText']),
-                    ...(questions[_questionIndex]['answers'] as List<String>)
-                        .map((answer) {
-                      return Answer(_answerQuestion, answer);
-                    }).toList()
-                  ],
-                )
-              : Center(child: Text('You did it!'))
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questions: _questions,
+                  questionIndex: _questionIndex)
+              : Result()
           // control + space_bar to cycle through suggested arguments
           ),
     );
